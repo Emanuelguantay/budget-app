f66f004772b2fc202b80088ff15e0bf39bc50942
eguantay <eguantay@silentiumapps.com>
2020-06-25 01:17:13 -0300

    Verificación formulario

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -10,7 +10,7 @@ export class FormComponent {
     description: string;
     valueAdd: number;
     optionSelected: string  = 'ing';
-
+    type : string = 'ing';
     constructor(
         private movementService: MovementService,
         ){
@@ -20,10 +20,14 @@ export class FormComponent {
     onAddValues(){
         let movement = new Movement(this.description, this.valueAdd);
         var option = this.optionSelected;
-        if (option == 'ing'){
+        if (this.type == 'ing'){
             this.movementService.addIncome(movement);
         }else{
             this.movementService.addExpense(movement);
         }
+    }
+
+    typeOperation(event){
+        this.type = event.target.value;
     }
 }
